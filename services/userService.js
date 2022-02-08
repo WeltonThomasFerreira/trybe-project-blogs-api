a6852e65fa719de0ef60b2ea54e279fc3096ba0c
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-08 04:07:06 -0300

    Move tudo para dentro do try...catch

--- services/userService.js
+++ services/userService.js
@@ -6,8 +6,8 @@ const {
 } = require('./errors');
 
 exports.validateDisplayName = async (displayName) => {
-  const schema = Joi.string().required().min(8).error(DISPLAY_NAME_INVALID);
   try {
+    const schema = Joi.string().required().min(8).error(DISPLAY_NAME_INVALID);
     await schema.validateAsync(displayName);
   } catch (error) {
     console.error(error);
@@ -16,11 +16,11 @@ exports.validateDisplayName = async (displayName) => {
 };
 
 exports.validateEmail = async (email) => {
-  const schema = Joi.object({
-    isRequired: Joi.string().required().error(EMAIL_IS_REQUIRED),
-    isValid: Joi.string().email().error(EMAIL_IS_INVALID),
-  });
   try {
+    const schema = Joi.object({
+      isRequired: Joi.string().required().error(EMAIL_IS_REQUIRED),
+      isValid: Joi.string().email().error(EMAIL_IS_INVALID),
+    });
     await schema.validateAsync({ isRequired: email, isValid: email });
   } catch (error) {
     console.error(error);
