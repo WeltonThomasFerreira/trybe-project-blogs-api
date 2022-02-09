c8afd3d918b8980dbf634dfc5eec0b9930f58c1c
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-08 21:44:46 -0300

    Add comentário para execução do script .sh

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -1,1 +1,4 @@
+# Para dar permisão de execução use o comando chmod 755 ./docker-compose_.sh
+# Execute o script com ./docker-compose-up.sh
+# É preciso estar dentro da pasta docker
 docker-compose up --force-recreate -d;
