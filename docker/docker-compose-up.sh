3c65ed6dbc1853b30d901d1c2effa09d45b45ac3
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-02-08 23:38:11 -0300

    Corrige comentário no script do docker

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -1,4 +1,4 @@
-# Para dar permisão de execução use o comando chmod 755 ./docker-compose_.sh
+# Para dar permisão de execução use o comando chmod 755 ./docker-compose-up.sh
 # Execute o script com ./docker-compose-up.sh
 # É preciso estar dentro da pasta docker
 docker-compose up --force-recreate -d;
